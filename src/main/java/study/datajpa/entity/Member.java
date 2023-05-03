e64bc3a32006a91522d326404403583420d1ca03
Kyounghan Ryu <thankryu@gmail.com>
2023-05-03 23:23:43 +0900

    feat : member jpa 로 조건 검색

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -42,6 +42,11 @@ public class Member {
         }
     }
 
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
     // 연관관계 셋팅
     public void changeTeam(Team team){
         this.team = team;
