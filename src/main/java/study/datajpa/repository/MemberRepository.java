f189190fd0ec32b3d6960f7b98d1b759fe14528f
Kyounghan Ryu <thankryu@gmail.com>
2023-05-04 10:24:10 +0900

    feat : NamedQuery 사용

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -10,4 +12,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
     List<Member> findTop3HelloBy();
+
+//    // 관례상 주석처리해도 돌아감. 실무에서 거의 안씀
+//    @Query(name = "Member.findByUsername")
+    List<Member> findByUsername(@Param("username") String username);
 }
