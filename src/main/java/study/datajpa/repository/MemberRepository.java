55c06a6e329b349d3d471ab7f09b7b1e17f1befd
Kyounghan Ryu <thankryu@gmail.com>
2023-05-04 17:47:37 +0900

    feat : 스프링 데이터 벌크 수정 쿼리 추가 - @Modifying 설정 주의 - JPQL 쿼리 순서 주의

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.dto.MemberDto;
@@ -49,5 +50,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Page<Member> findByAge(int age, Pageable pageable);
     // Slice<Member> findByAge(int age, Pageable pageable);
     // List<Member> findByAge(int age, Pageable pageable);
+
+    // Modifying 변경시에 필요
+    @Modifying(clearAutomatically = true)
+    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
 
 }
