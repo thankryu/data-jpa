a41d7f27ced0ce28722476424ab5747e8a0bfbca
Kyounghan Ryu <thankryu@gmail.com>
2023-05-07 12:02:02 +0900

    feat : Jpa Hint 추가
    - 성능 최적화를 위한 select only

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,14 +3,12 @@ package study.datajpa.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
+import javax.persistence.QueryHint;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
@@ -74,4 +72,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @EntityGraph("Member.all")
     List<Member> findEntityGraphNamedByUsername(@Param("username") String username);
+
+    // 읽기로만 쓰는것으로 최적화가 되어 변경이 안됨
+    @QueryHints(value = @QueryHint( name = "org.hibernate.readOnly", value = "true"))
+    Member findReadOnlyByUsername(String username);
 }
