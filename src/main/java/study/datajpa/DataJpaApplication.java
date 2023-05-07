996bc253d9b98b2f0a7936f6df049cab24b8d05c
Kyounghan Ryu <thankryu@gmail.com>
2023-05-07 12:54:32 +0900

    feat : 스프링 데이터 Auditing 추가
    - @EnableJpaAuditing 설정 확인!

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,12 +2,26 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+// EnableJpaAuditing 주의
+@EnableJpaAuditing
 @SpringBootApplication
 public class DataJpaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DataJpaApplication.class, args);
+    }
+
+    @Bean
+    public AuditorAware<String> auditorProvider(){
+        // 실무에서는 spring security, session 등에서 id 등을 꺼내서집어넣어서 사용한다
+        return () -> Optional.of(UUID.randomUUID().toString());
     }
 
 }
