c34ff41943ddbe50037724f0c8b3999cad87d260
Kyounghan Ryu <thankryu@gmail.com>
2023-05-07 15:11:25 +0900

    feat : 스프링 데이타 페이징 및 정렬
    - Dto 반환 주의

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,13 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -32,9 +36,32 @@ public class MemberController {
     public String findMember2(@PathVariable("id") Member member){
         return member.getUsername();
     }
+    
+//   // Entity를 그대로 노출하는 경우 -> 비추천
+//    @GetMapping("/members")
+//    public Page<Member> list(@PageableDefault(size = 5) Pageable pageable){
+//        Page<Member> page = memberRepository.findAll(pageable);
+//        return page;
+//    }
+
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable){
+        // 1. pageable 로 entity 값 조회
+        // 2. entity 값 -> Dto 에 담아서 반환
+        // 2-1. 방법1
+//        return memberRepository.findAll(pageable)
+//                .map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+
+        // 2-2. 방법2 Dto 에서 선언 후 가능
+        return memberRepository.findAll(pageable)
+                .map(MemberDto::new);
+    }
+
 
     @PostConstruct
     public void init(){
-        memberRepository.save(new Member("userA"));
+        for(int i =0; i < 100; i++){
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
